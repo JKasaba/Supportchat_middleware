fb444a63406e81c583904f721c2ff54572f2e28c
James Kasaba <jamesk@filmlight.ltd.uk>
2025-05-01 15:33:38 -0700

    clean up main.py

--- main.py
+++ main.py
@@ -1,20 +1,3 @@
-# from flask import Flask, request, jsonify
-
-# app = Flask(__name__)
-
-# @app.route("/webhook/whatsapp", methods=["POST"])
-# def handle_webhook():
-#     data = request.get_json(force=True)
-#     print("📩 Received WhatsApp webhook:", data)
-#     return jsonify({"status": "received"}), 200
-
-# @app.route("/", methods=["GET"])
-# def index():
-#     return "Flask WhatsApp webhook is running!", 200
-
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5000)
-
 import requests
 from flask import Flask, request, jsonify
 import os
